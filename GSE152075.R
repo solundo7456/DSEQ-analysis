b72b85e4e476adbbffc3b96a281fa806e47d7df6
Annah Ndono <annahndono7456@gmail.com>
2025-11-12 22:32:30 +0300

    Implement gene enrichment analysis features
    Added functionality for gene enrichment analysis including GO and KEGG pathways.

--- GSE152075.R
+++ GSE152075.R
@@ -33,7 +33,7 @@ experimentData(gse)
 head(counts)
 dim(counts)
 #create sample list
-meta <- pData(gse[[1]])
+meta <- pData(gse)
 head(meta)
 #ensure columns of counts match rowa of metadata
 all(colnames(counts) %in% rownames(meta$title))
@@ -69,12 +69,15 @@ meta2$`sequencing_batch` <- factor(meta2$sequencing_batch)
 meta2$gender <- factor(meta2$gender)
 #handling numeric
 meta2$age <- as.numeric(meta2$age)
-meta2[is.na(meta2$age), "age"]
+unique(meta2$age)
 meta2$age[meta2$age %in% c("NA")] <- NA 
 meta2$age <- as.numeric(meta2$age)
-meta2$viral_load <- as.numeric(meta2$viral_load)
 
+unique(meta2$viral_load)
+meta2$viral_load[meta2$viral_load %in% c("NA")] <- NA 
+meta2$viral_load <- as.numeric(meta2$viral_load)
 #reorder
+
 head(colnames(counts_filtered))
 head(rownames(meta2))
 rownames(meta2) <- meta2$title 
@@ -117,3 +120,74 @@ colnames(meta2)
 pheatmap(mat, scale="row", annotation_col = meta2[, c("infection_status","gender")])
 write.csv(as.data.frame(res), "DE_results_Positive_vs_Negative.csv")
 
+#1. functionality
+# Install necessary packages if not installed
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+
+
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(ReactomePA)
+library(enrichplot)
+
+# 2. Filter significant genes
+# -----------------------------
+sig_genes <- rownames(res)[which(res$padj < 0.05 & abs(res$log2FoldChange) > 1)]
+length(sig_genes)
+head(sig_genes)
+sig_genes
+# 3. Map gene symbols to Entrez IDs
+# -----------------------------
+entrez_ids <- bitr(sig_genes,
+                   fromType = "SYMBOL",
+                   toType = "ENTREZID",
+                   OrgDb = org.Hs.eg.db)
+head(entrez_ids)
+
+# 4. GO enrichment (Biological Process)
+# -----------------------------
+ego <- enrichGO(gene         = entrez_ids$ENTREZID,
+                OrgDb        = org.Hs.eg.db,
+                keyType      = "ENTREZID",
+                ont          = "BP",
+                pAdjustMethod= "BH",
+                pvalueCutoff = 0.05,
+                qvalueCutoff = 0.05)
+
+ego_1 <- enrichGO(gene         = entrez_ids$ENTREZID,
+                OrgDb        = org.Hs.eg.db,
+                keyType      = "ENTREZID",
+                ont          = "MF",
+                pAdjustMethod= "BH",
+                pvalueCutoff = 0.05,
+                qvalueCutoff = 0.05)
+# Top GO terms
+head(ego)
+head(ego_1)
+# Dotplot
+dotplot(ego, showCategory=20) + ggtitle("GO Biological Process Enrichment")
+
+# 5. KEGG pathway enrichment
+# -----------------------------
+options(timeout = 600) 
+ekegg <- enrichKEGG(gene         = entrez_ids$ENTREZID,
+                    organism     = "hsa",
+                    pvalueCutoff = 0.05)
+
+# Top KEGG pathways
+head(ekegg)
+dotplot(ekegg, showCategory=20) + ggtitle("KEGG Pathway Enrichment")
+
+# -----------------------------
+# 6. Reactome pathway enrichment
+# -----------------------------
+ereact <- enrichPathway(gene=entrez_ids$ENTREZID,
+                        organism="human",
+                        pvalueCutoff=0.05,
+                        readable=TRUE)
+
+# Top Reactome pathways
+head(ereact)
+dotplot(ereact, showCategory=20) + ggtitle("Reactome Pathway Enrichment")
